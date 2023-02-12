77e3c8164c1074f00a19b67afcb818dc3afc24c5
lavindude <lavindude@gmail.com>
2023-02-12 12:31:10 -0800

    basic login and signup functionality is complete, need to clean/organize some things now

---
+++ app/utils/auth/signup.ts
@@ -0,0 +1,13 @@
+import { APIURL } from "../api/APIConstants";
+
+export const signup = async (name: String, email: String, password: String) => {
+    // TODO: filter inputs**
+
+    const response = await fetch(APIURL + `/auth/signup?name=${name}&email=${email}&password=${password}`, {
+        method: 'POST'
+    });
+    
+    const myJson = await response.json(); //extract JSON from the http response
+
+    return myJson;
+}
