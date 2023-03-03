a3c249c3baa8825fcffeb6424dd9d695c1fdfdca
naotost <naotost@cs.washington.edu>
2023-03-03 15:54:12 -0800

    shape works now

--- app/screens/Tabs/Library.tsx
+++ app/screens/Tabs/Library.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, Image} from 'react-native';
 import {colors} from "../../constants/colors";
 import Hat from "../../assets/vectors/hat.svg";
-
-
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
 
 const Library = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.background}>
-        <Hat width="100%" height="100%" fill={colors.fill} originY={0}/>
+      <View style={{ width: "100%", aspectRatio: 1, position: "absolute", top: 0}} >
+        <Hat width="100%" height="100%" fill={colors.fill}/>
       </View>
-      {/* <Text style={styles.text}>Coming Soon</Text> */}
+      <Text style={styles.title}>Library</Text>
     </View>
   );
 };
@@ -23,21 +19,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    width: screenWidth,
-    height: screenHeight,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
     backgroundColor: colors.white,
   },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  title: {
     position: "absolute",
-    top: 40,
+    top: 60,
+    left: 35,
+
+    // font-family: 'Lato;,
+    fontSize: 32,
+    fontWeight: 'bold',
+    
+    color: colors.black,
   },
-  background: {
-    position: 'absolute',
-    width: "100%",
-    height: "100%",
-  }
 });
 
 export default Library;
