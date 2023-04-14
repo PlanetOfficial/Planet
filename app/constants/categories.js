fddf213bb526f6f63267977b16d9da863d526dc3
naotost <naotost@cs.washington.edu>
2023-04-14 15:37:22 -0700

    added subcategories to food & beverages

--- app/constants/categories.js
+++ app/constants/categories.js
@@ -9,7 +9,9 @@ export const genres = [
         name: 'Music',
         supplier: 'ticketmaster',
         alias: 'KZFzniwnSyZfZ7v7nJ',
-        icon: require('../assets/icons/music.png'),
+        subcategories: [
+            
+        ]
       },
       {
         id: 2,
@@ -41,18 +43,729 @@ export const genres = [
         name: 'Restaurant',
         supplier: 'yelp',
         alias: 'restaurants',
+        subcategories: [
+          {
+            "id": 1,
+            "alias": "afghani",
+            "title": "Afghan"
+          },
+          {
+            "id": 2,
+            "alias": "african",
+            "title": "African"
+          },
+          {
+            "id": 3,
+            "alias": "arabian",
+            "title": "Arabic"
+          },
+          {
+            "id": 4,
+            "alias": "argentine",
+            "title": "Argentine"
+          },
+          {
+            "id": 5,
+            "alias": "armenian",
+            "title": "Armenian"
+          },
+          {
+            "id": 6,
+            "alias": "asianfusion",
+            "title": "Asian Fusion"
+          },
+          {
+            "id": 7,
+            "alias": "australian",
+            "title": "Australian"
+          },
+          {
+            "id": 8,
+            "alias": "austrian",
+            "title": "Austrian"
+          },
+          {
+            "id": 9,
+            "alias": "bangladeshi",
+            "title": "Bangladeshi"
+          },
+          {
+            "id": 10,
+            "alias": "basque",
+            "title": "Basque"
+          },
+          {
+            "id": 11,
+            "alias": "bbq",
+            "title": "Barbeque"
+          },
+          {
+            "id": 12,
+            "alias": "belgian",
+            "title": "Belgian"
+          },
+          {
+            "id": 13,
+            "alias": "brasseries",
+            "title": "Brasseries"
+          },
+          {
+            "id": 14,
+            "alias": "brazilian",
+            "title": "Brazilian"
+          },
+          {
+            "id": 15,
+            "alias": "breakfast_brunch",
+            "title": "Breakfast & Brunch"
+          },
+          {
+            "id": 16,
+            "alias": "british",
+            "title": "British"
+          },
+          {
+            "id": 17,
+            "alias": "buffets",
+            "title": "Buffets"
+          },
+          {
+            "id": 18,
+            "alias": "bulgarian",
+            "title": "Bulgarian"
+          },
+          {
+            "id": 19,
+            "alias": "burgers",
+            "title": "Burgers"
+          },
+          {
+            "id": 20,
+            "alias": "burmese",
+            "title": "Burmese"
+          },
+          {
+            "id": 21,
+            "alias": "cajun",
+            "title": "Cajun/Creole"
+          },
+          {
+            "id": 22,
+            "alias": "cambodian",
+            "title": "Cambodian"
+          },
+          {
+            "id": 23,
+            "alias": "caribbean",
+            "title": "Caribbean"
+          },
+          {
+            "id": 24,
+            "alias": "catalan",
+            "title": "Catalan"
+          },
+          {
+            "id": 25,
+            "alias": "chech",
+            "title": "Chech"
+          },
+          {
+            "id": 26,
+            "alias": "cheesesteaks",
+            "title": "Cheesesteaks"
+          },
+          {
+            "id": 27,
+            "alias": "chicken_wings",
+            "title": "Chicken Wings"
+          },
+          {
+            "id": 28,
+            "alias": "chickenshop",
+            "title": "Chicken Shop"
+          },
+          {
+            "id": 29,
+            "alias": "chinese",
+            "title": "Chinese"
+          },
+          {
+            "id": 30,
+            "alias": "comfortfood",
+            "title": "Comfort Food"
+          },
+          {
+            "id": 31,
+            "alias": "cuban",
+            "title": "Cuban"
+          },
+          {
+            "id": 32,
+            "alias": "czech",
+            "title": "Czech"
+          },
+          {
+            "id": 33,
+            "alias": "delis",
+            "title": "Delis"
+          },
+          {
+            "id": 34,
+            "alias": "diners",
+            "title": "Diners"
+          },
+          {
+            "id": 35,
+            "alias": "dinnertheater",
+            "title": "Dinner Theater"
+          },
+          {
+            "id": 36,
+            "alias": "eritrean",
+            "title": "Eritrean"
+          },
+          {
+            "id": 37,
+            "alias": "ethiopian",
+            "title": "Ethiopian"
+          },
+          {
+            "id": 38,
+            "alias": "filipino",
+            "title": "Filipino"
+          },
+          {
+            "id": 39,
+            "alias": "fishnchips",
+            "title": "Fish & Chips"
+          },
+          {
+            "id": 40,
+            "alias": "fondue",
+            "title": "Fondue"
+          },
+          {
+            "id": 41,
+            "alias": "food_court",
+            "title": "Food Court"
+          },
+          {
+            "id": 42,
+            "alias": "foodstands",
+            "title": "Food Stands"
+          },
+          {
+            "id": 43,
+            "alias": "french",
+            "title": "French"
+          },
+          {
+            "id": 44,
+            "alias": "gamemeat",
+            "title": "Game Meat"
+          },
+          {
+            "id": 45,
+            "alias": "gastropubs",
+            "title": "Gastropubs"
+          },
+          {
+            "id": 46,
+            "alias": "georgian",
+            "title": "Georgian"
+          },
+          {
+            "id": 47,
+            "alias": "german",
+            "title": "German"
+          },
+          {
+            "id": 48,
+            "alias": "gluten_free",
+            "title": "Gluten-Free"
+          },
+          {
+            "id": 49,
+            "alias": "greek",
+            "title": "Greek"
+          },
+          {
+            "id": 50,
+            "alias": "guamanian",
+            "title": "Guamanian"
+          },
+          {
+            "id": 51,
+            "alias": "halal",
+            "title": "Halal"
+          },
+          {
+            "id": 52,
+            "alias": "hawaiian",
+            "title": "Hawaiian"
+          },
+          {
+            "id": 53,
+            "alias": "himalayan",
+            "title": "Himalayan/Nepalese"
+          },
+          {
+            "id": 54,
+            "alias": "hkcafe",
+            "title": "Hong Kong Style Cafe"
+          },
+          {
+            "id": 55,
+            "alias": "honduran",
+            "title": "Honduran"
+          },
+          {
+            "id": 56,
+            "alias": "hotdog",
+            "title": "Hot Dogs"
+          },
+          {
+            "id": 57,
+            "alias": "hotdogs",
+            "title": "Fast Food"
+          },
+          {
+            "id": 58,
+            "alias": "hotpot",
+            "title": "Hot Pot"
+          },
+          {
+            "id": 59,
+            "alias": "hungarian",
+            "title": "Hungarian"
+          },
+          {
+            "id": 60,
+            "alias": "iberian",
+            "title": "Iberian"
+          },
+          {
+            "id": 61,
+            "alias": "indonesian",
+            "title": "Indonesian"
+          },
+          {
+            "id": 62,
+            "alias": "indpak",
+            "title": "Indian"
+          },
+          {
+            "id": 63,
+            "alias": "irish",
+            "title": "Irish"
+          },
+          {
+            "id": 64,
+            "alias": "italian",
+            "title": "Italian"
+          },
+          {
+            "id": 65,
+            "alias": "japanese",
+            "title": "Japanese"
+          },
+          {
+            "id": 66,
+            "alias": "kebab",
+            "title": "Kebab"
+          },
+          {
+            "id": 67,
+            "alias": "korean",
+            "title": "Korean"
+          },
+          {
+            "id": 68,
+            "alias": "kosher",
+            "title": "Kosher"
+          },
+          {
+            "id": 69,
+            "alias": "laotian",
+            "title": "Laotian"
+          },
+          {
+            "id": 70,
+            "alias": "latin",
+            "title": "Latin American"
+          },
+          {
+            "id": 71,
+            "alias": "malaysian",
+            "title": "Malaysian"
+          },
+          {
+            "id": 72,
+            "alias": "mediterranean",
+            "title": "Mediterranean"
+          },
+          {
+            "id": 73,
+            "alias": "mexican",
+            "title": "Mexican"
+          },
+          {
+            "id": 74,
+            "alias": "mideastern",
+            "title": "Middle Eastern"
+          },
+          {
+            "id": 75,
+            "alias": "modern_european",
+            "title": "Modern European"
+          },
+          {
+            "id": 76,
+            "alias": "mongolian",
+            "title": "Mongolian"
+          },
+          {
+            "id": 77,
+            "alias": "moroccan",
+            "title": "Moroccan"
+          },
+          {
+            "id": 78,
+            "alias": "newamerican",
+            "title": "American (New)"
+          },
+          {
+            "id": 79,
+            "alias": "newmexican",
+            "title": "New Mexican Cuisine"
+          },
+          {
+            "id": 80,
+            "alias": "nicaraguan",
+            "title": "Nicaraguan"
+          },
+          {
+            "id": 81,
+            "alias": "noodles",
+            "title": "Noodles"
+          },
+          {
+            "id": 82,
+            "alias": "pakistani",
+            "title": "Pakistani"
+          },
+          {
+            "id": 83,
+            "alias": "panasian",
+            "title": "Pan Asian"
+          },
+          {
+            "id": 84,
+            "alias": "persian",
+            "title": "Persian/Iranian"
+          },
+          {
+            "id": 85,
+            "alias": "peruvian",
+            "title": "Peruvian"
+          },
+          {
+            "id": 86,
+            "alias": "pizza",
+            "title": "Pizza"
+          },
+          {
+            "id": 87,
+            "alias": "polish",
+            "title": "Polish"
+          },
+          {
+            "id": 88,
+            "alias": "polynesian",
+            "title": "Polynesian"
+          },
+          {
+            "id": 89,
+            "alias": "popuprestaurants",
+            "title": "Pop-Up Restaurants"
+          },
+          {
+            "id": 90,
+            "alias": "portuguese",
+            "title": "Portuguese"
+          },
+          {
+            "id": 91,
+            "alias": "poutineries",
+            "title": "Poutineries"
+          },
+          {
+            "id": 92,
+            "alias": "raw_food",
+            "title": "Live/Raw Food"
+          },
+          {
+            "id": 93,
+            "alias": "rumanian",
+            "title": "Rumanian"
+          },
+          {
+            "id": 94,
+            "alias": "russian",
+            "title": "Russian"
+          },
+          {
+            "id": 95,
+            "alias": "salad",
+            "title": "Salad"
+          },
+          {
+            "id": 96,
+            "alias": "sandwiches",
+            "title": "Sandwiches"
+          },
+          {
+            "id": 97,
+            "alias": "scandinavian",
+            "title": "Scandinavian"
+          },
+          {
+            "id": 98,
+            "alias": "scottish",
+            "title": "Scottish"
+          },
+          {
+            "id": 99,
+            "alias": "seafood",
+            "title": "Seafood"
+          },
+          {
+            "id": 100,
+            "alias": "singaporean",
+            "title": "Singaporean"
+          },
+          {
+            "id": 101,
+            "alias": "slovakian",
+            "title": "Slovakian"
+          },
+          {
+            "id": 102,
+            "alias": "somali",
+            "title": "Somali"
+          },
+          {
+            "id": 103,
+            "alias": "soulfood",
+            "title": "Soul Food"
+          },
+          {
+            "id": 104,
+            "alias": "soup",
+            "title": "Soup"
+          },
+          {
+            "id": 105,
+            "alias": "southern",
+            "title": "Southern"
+          },
+          {
+            "id": 106,
+            "alias": "spanish",
+            "title": "Spanish"
+          },
+          {
+            "id": 107,
+            "alias": "srilankan",
+            "title": "Sri Lankan"
+          },
+          {
+            "id": 108,
+            "alias": "steak",
+            "title": "Steakhouses"
+          },
+          {
+            "id": 109,
+            "alias": "supperclubs",
+            "title": "Supper Clubs"
+          },
+          {
+            "id": 110,
+            "alias": "sushi",
+            "title": "Sushi Bars"
+          },
+          {
+            "id": 111,
+            "alias": "syrian",
+            "title": "Syrian"
+          },
+          {
+            "id": 112,
+            "alias": "taiwanese",
+            "title": "Taiwanese"
+          },
+          {
+            "id": 113,
+            "alias": "tapas",
+            "title": "Tapas Bars"
+          },
+          {
+            "id": 114,
+            "alias": "tapasmallplates",
+            "title": "Tapas/Small Plates"
+          },
+          {
+            "id": 115,
+            "alias": "tex-mex",
+            "title": "Tex-Mex"
+          },
+          {
+            "id": 116,
+            "alias": "thai",
+            "title": "Thai"
+          },
+          {
+            "id": 117,
+            "alias": "tradamerican",
+            "title": "American (Traditional)"
+          },
+          {
+            "id": 118,
+            "alias": "turkish",
+            "title": "Turkish"
+          },
+          {
+            "id": 119,
+            "alias": "ukrainian",
+            "title": "Ukrainian"
+          },
+          {
+            "id": 120,
+            "alias": "uzbek",
+            "title": "Uzbek"
+          },
+          {
+            "id": 121,
+            "alias": "vegan",
+            "title": "Vegan"
+          },
+          {
+            "id": 122,
+            "alias": "vegetarian",
+            "title": "Vegetarian"
+          },
+          {
+            "id": 123,
+            "alias": "vietnamese",
+            "title": "Vietnamese"
+          },
+          {
+            "id": 124,
+            "alias": "wraps",
+            "title": "Wraps"
+          }
+        ]
       },
       {
         id: 6,
         name: 'Dessert',
         supplier: 'yelp',
-        alias: 'desserts',
+        alias: 'desserts,bakeries,cakeshop,chimneycakes,cupcakes,donuts,gelato,icecream,bubbletea,juicebar,shavedice,shavedsnow,creperies,waffles',
+        subcategories: [
+          {
+            "id": 1,
+            "alias": "bakeries",
+            "title": "Bakeries"
+          },
+          {
+            "id": 2,
+            "alias": "cakeshop",
+            "title": "Patisserie/Cake Shop"
+          },
+          {
+            "id": 3,
+            "alias": "chimneycakes",
+            "title": "Chimney Cakes"
+          },
+          {
+            "id": 4,
+            "alias": "cupcakes",
+            "title": "Cupcakes"
+          },
+          {
+            "id": 5,
+            "alias": "donuts",
+            "title": "Donuts"
+          },
+          {
+            "id": 6,
+            "alias": "gelato",
+            "title": "Gelato"
+          },
+          {
+            "id": 7,
+            "alias": "icecream",
+            "title": "Ice Cream & Frozen Yogurt"
+          },
+          {
+            "id": 8,
+            "alias": "bubbletea",
+            "title": "Bubble Tea"
+          },
+          {
+            "id": 9,
+            "alias": "juicebars",
+            "title": "Juice Bars & Smoothies"
+          },
+          {
+            "id": 10,
+            "alias": "shavedice,shavedsnow",
+            "title": "Shaved Ice"
+          },
+          {
+            "id": 11,
+            "alias": "creperies",
+            "title": "Creperies"
+          },
+          {
+            "id": 12,
+            "alias": "waffles",
+            "title": "Waffles"
+          },
+        ]
       },
       {
         id: 7,
-        name: 'Coffee & Tea',
+        name: 'Cafe',
         supplier: 'yelp',
-        alias: 'coffee',
+        alias: 'cafe,coffee,bubbletea,internetcafe,tea',
+        subcategories: [
+          {
+            "id": 1,
+            "alias": "cafes",
+            "title": "Cafes",
+          },
+          {
+            "id": 2,
+            "alias": "coffee",
+            "title": "Coffee & Tea",
+          },
+          {
+            "id": 3,
+            "alias": "bubbletea",
+            "title": "Bubble Tea"
+          },
+          {
+            "id": 4,
+            "alias": "internetcafe",
+            "title": "Internet Cafes"
+          },
+          {
+            "id": 5,
+            "alias": "tea",
+            "title": "Tea Rooms"
+          },
+        ]
       },
       {
         id: 8,
