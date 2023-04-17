624aaf66324bd05949df152ddf93db5f4ebf38d3
naotost <naotost@cs.washington.edu>
2023-04-16 20:06:13 -0700

    renamed trending file

--- app/ui/components/NavBar.tsx
+++ app/ui/components/NavBar.tsx
@@ -7,7 +7,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {colors} from '../../constants/theme';
 import {tabIcons} from '../../constants/images';
 
-import Trending from '../tabs/!Trending';
+import Trending from '../tabs/Trending';
 import Friends from '../tabs/Friends';
 import Library from '../tabs/Library';
 import Profile from '../tabs/Profile';
