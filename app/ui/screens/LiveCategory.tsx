7a10580a38d4e144b8721e9fb8b6462b89c5dbc3
naotost <naotost@cs.washington.edu>
2023-04-16 22:15:34 -0700

    still have to make the header fancy

--- app/ui/screens/LiveCategory.tsx
+++ app/ui/screens/LiveCategory.tsx
@@ -15,10 +15,37 @@ import {s} from 'react-native-size-matters';
 import {genres} from '../../constants/genres';
 import PlaceCard from '../components/PlaceCard';
 
+const TEMP_DATA = [
+  {
+    id: '1',
+    name: 'The Witcher',
+    image: 'https://image.tmdb.org/t/p/w500/2W4ZvACURDyhiNnSIaFPHfNbny3.jpg',
+    rating: 8.5,
+    date: '2020-12-25',
+    marked: false,
+  },
+  {
+    id: '2',
+    name: 'The Mandalorian',
+    image: 'https://image.tmdb.org/t/p/w500/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg',
+    rating: 8.5,
+    date: '2020-12-25',
+    marked: true,
+  },
+  {
+    id: '3',
+    name: 'The Witcher',
+    image: 'https://image.tmdb.org/t/p/w500/2W4ZvACURDyhiNnSIaFPHfNbny3.jpg',
+    rating: 8.5,
+    date: '2020-12-25',
+    marked: true,
+  },
+];
+
 const LiveCategory = ({navigation}: {navigation: any}) => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={headerStyles.container}>
+    <View style={styles.container}>
+      <SafeAreaView style={headerStyles.container}>
         <TouchableOpacity onPress={() => navigation.navigate('Trending')}>
           <Image style={headerStyles.back} source={icons.next} />
         </TouchableOpacity>
@@ -26,15 +53,56 @@ const LiveCategory = ({navigation}: {navigation: any}) => {
         <TouchableOpacity onPress={() => console.log('Customize Screen')}>
           <Image style={headerStyles.settings} source={icons.settings} />
         </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+      <ScrollView>
+        {genres[0].categories[0].subcategories?.map((category, idx) => (
+          <View key={idx} style={categoryStyles.container}>
+            <View style={categoryStyles.header}>
+              <Text style={categoryStyles.title}>{category.title}</Text>
+            </View>
+            <ScrollView
+              contentContainerStyle={categoryStyles.contentContainer}
+              style={categoryStyles.scrollView}
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}>
+              {TEMP_DATA.map((item: any, jdx) => (
+                <TouchableOpacity
+                  style={categoryStyles.card}
+                  key={jdx}
+                  onPress={() => console.log("Lavy's backend magic")}>
+                  <PlaceCard
+                    id={item?.id}
+                    name={item?.name}
+                    info={item?.date}
+                    marked={item?.marked}
+                    image={{uri: item?.image}}
+                  />
+                </TouchableOpacity>
+              ))}
+            </ScrollView>
+            <Spacer />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
   );
 };
+
+const Spacer = () => <View style={styles.separator} />;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.white,
+  },
+  separator: {
+    borderWidth: 0.5,
+    borderColor: colors.grey,
+    marginLeft: s(20),
+    marginTop: s(10),
+  },
+  bottomPadding: {
+    height: s(20),
   },
 });
 
@@ -44,23 +112,52 @@ const headerStyles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     marginHorizontal: s(20),
-    marginVertical: s(15),
   },
   title: {
-    fontSize: s(20),
+    fontSize: s(18),
     fontWeight: '700',
     color: colors.black,
+    marginVertical: s(15),
   },
   back: {
     width: s(12),
     height: s(18),
+    marginRight: s(6),
     tintColor: colors.black,
     transform: [{rotate: '180deg'}],
   },
   settings: {
-    width: s(20),
-    height: s(20),
+    width: s(18),
+    height: s(18),
     tintColor: colors.black,
+  },
+});
+
+const categoryStyles = StyleSheet.create({
+  container: {
+    marginTop: s(10),
+  },
+  header: {
+    marginHorizontal: s(20),
+    marginBottom: s(10),
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: s(16),
+    fontWeight: '700',
+    color: colors.black,
+  },
+  card: {
+    width: s(225),
+    marginRight: s(10),
+  },
+  scrollView: {
+    overflow: 'visible',
+  },
+  contentContainer: {
+    paddingLeft: s(20),
+    paddingRight: s(5),
   },
 });
 
