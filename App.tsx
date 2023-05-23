b26fcac722c8fa84d9f3e7fc2205c38a5911da61
lavindude <lavindude@gmail.com>
2023-05-22 20:27:30 -0700

    iOS FE mostly implemented, background and foreground notifications now working

--- App.tsx
+++ App.tsx
@@ -1,13 +1,32 @@
 import 'react-native-gesture-handler';
 import React, {useEffect, useState} from 'react';
-import SplashScreen from './app/ui/otherScreens/!SplashScreen';
 import EncryptedStorage from 'react-native-encrypted-storage';
+import messaging from '@react-native-firebase/messaging';
+import {Alert, PermissionsAndroid} from 'react-native';
+import { Platform } from 'react-native';
+
+import SplashScreen from './app/ui/otherScreens/!SplashScreen';
 import AppNavigation from './app/navigation/AppNavigation';
 import {updateCaches} from './app/utils/functions/CacheHelpers';
 
 export default function App() {
   const [isLoading, setLoading] = useState<boolean>(true);
   const [isLoggedIn, setLoggedIn] = useState<boolean>(false);
+
+  const requestUserPermission = async() => {
+    if (Platform.OS === 'android') {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    } else {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    
+      if (enabled) {
+        console.log('Notifications authorized:', enabled);
+      }
+    }
+  }
 
   useEffect(() => {
     const initialize = async () => {
@@ -21,9 +40,16 @@ export default function App() {
       }
 
       setLoading(false);
+      requestUserPermission();
     };
 
     initialize();
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
   }, []);
 
   const getCorrectStack = () => {
