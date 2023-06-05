f8472153f297abea249d8637111dcb1e4a2fa575
naotost <naotost@cs.washington.edu>
2023-06-04 18:55:56 -0700

    created a stylesheet

---
+++ app/constants/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import colors from './colors';
+import {s} from 'react-native-size-matters';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  separator: {
+    marginHorizontal: s(20),
+    height: 0.5,
+    backgroundColor: colors.grey,
+  }
+});
+
+export default styles;
