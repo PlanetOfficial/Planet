aa62a4748c770c1bf24463f1e70706d1d65b307a
naotost <naotost@cs.washington.edu>
2023-07-08 18:25:05 -0700

    added buttons on friend search

---
+++ app/ui/friendsScreens/user/ActionButtons.tsx
@@ -0,0 +1,118 @@
+import React, {useContext} from 'react';
+import {StyleSheet, useColorScheme} from 'react-native';
+import {s} from 'react-native-size-matters';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+import colors from '../../../constants/colors';
+import strings from '../../../constants/strings';
+
+import Text from '../../components/Text';
+
+import {UserInfo} from '../../../utils/types';
+import FriendsContext from '../../../context/FriendsContext';
+import {
+  handleAcceptRequest,
+  handleCancelRequest,
+  handleDeclineRequest,
+  handleFriendRequest,
+  handleUnfriend,
+} from './functions';
+
+interface Props {
+  user: UserInfo;
+}
+
+const ActionButtons: React.FC<Props> = ({user}) => {
+  const theme = useColorScheme() || 'light';
+
+  const friendsContext = useContext(FriendsContext);
+  if (!friendsContext) {
+    throw new Error('FriendsContext is not set!');
+  }
+  const {
+    friends,
+    setFriends,
+    requests,
+    setRequests,
+    requestsSent,
+    setRequestsSent,
+  } = friendsContext;
+
+  return friends.some(friend => friend.id === user.id) ? (
+    <TouchableOpacity
+      style={{
+        ...styles.button,
+        backgroundColor: colors[theme].secondary,
+      }}
+      onPress={() => handleUnfriend(user.id, friends, setFriends)}>
+      <Text size="xs">{strings.friends.unfriend}</Text>
+    </TouchableOpacity>
+  ) : requestsSent.some(request => request.id === user.id) ? (
+    <TouchableOpacity
+      style={{
+        ...styles.button,
+        backgroundColor: colors[theme].secondary,
+      }}
+      onPress={() =>
+        handleCancelRequest(user.id, requestsSent, setRequestsSent)
+      }>
+      <Text size="xs">{strings.friends.cancelRequest}</Text>
+    </TouchableOpacity>
+  ) : requests.some(request => request.id === user.id) ? (
+    <>
+      <TouchableOpacity
+        style={{
+          ...styles.button,
+          backgroundColor: colors[theme].accent,
+        }}
+        onPress={() =>
+          handleAcceptRequest(
+            user.id,
+            friends,
+            setFriends,
+            requests,
+            setRequests,
+            user,
+          )
+        }>
+        <Text size="xs" color={colors[theme].primary}>
+          {strings.friends.accept}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          ...styles.button,
+          backgroundColor: colors[theme].secondary,
+        }}
+        onPress={() => handleDeclineRequest(user.id, requests, setRequests)}>
+        <Text size="xs">{strings.friends.reject}</Text>
+      </TouchableOpacity>
+    </>
+  ) : (
+    <TouchableOpacity
+      style={{
+        ...styles.button,
+        backgroundColor: colors[theme].accent,
+      }}
+      onPress={() =>
+        handleFriendRequest(user.id, requestsSent, setRequestsSent, user)
+      }>
+      <Text size="xs" color={colors[theme].primary}>
+        {strings.friends.addFriend}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    paddingHorizontal: s(10),
+    paddingVertical: s(5),
+    borderRadius: s(5),
+    marginRight: s(10),
+    minWidth: s(65),
+    alignItems: 'center',
+  },
+});
+
+export default ActionButtons;
