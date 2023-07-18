c1abad263a1a9ecf66b0cfadd47993ca20fd6749
kvnwxng0 <kvnwxng@gmail.com>
2023-07-17 23:41:33 -0500

    added splash to launchimage

--- app/ui/otherScreens/splashScreen/SplashScreen.tsx
+++ app/ui/otherScreens/splashScreen/SplashScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, useColorScheme, StatusBar} from 'react-native';
+import {View, StyleSheet, useColorScheme, StatusBar, Image} from 'react-native';
 
 import colors from '../../../constants/colors';
 
@@ -8,7 +8,11 @@ const SplashScreen = () => {
   const styles = styling(theme);
   StatusBar.setBarStyle(colors[theme].statusBar, true);
 
-  return <View style={styles.background} />;
+  return (
+    <View style={styles.background}>
+      <Image style={styles.image} source={require('../../../assets/images/launchimage.png')} />
+    </View>
+  );
 };
 
 const styling = (theme: 'light' | 'dark') =>
@@ -16,6 +20,14 @@ const styling = (theme: 'light' | 'dark') =>
     background: {
       flex: 1,
       backgroundColor: colors[theme].neutral,
+      justifyContent: 'center', // center the image vertically
+      alignItems: 'center', // center the image horizontally
+    },
+    image: {
+      flex: 1,
+      width: '100%',
+      height: '100%',
+      resizeMode: 'cover',
     },
   });
 
